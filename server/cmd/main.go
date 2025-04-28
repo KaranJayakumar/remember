42ec8a9aca216bd0f2ec03eae0979a5451606120
KaranJayakumar <karanjaykumar@gmail.com>
2025-04-28 22:37:57 +1000

    Move main.go into cmd dir

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "context"
-    "log"
-
-    "entdemo/ent"
-
-    _ "github.com/lib/pq"
+	"context"
+	"github.com/KaranJayakumar/remember/ent"
+	"log"
+	"os"
 )
 
 func main() {
-    client, err := ent.Open("postgres","host=<host> port=<port> user=<user> dbname=<database> password=<pass>")
-    if err != nil {
-        log.Fatalf("failed opening connection to postgres: %v", err)
-    }
-    defer client.Close()
-    if err := client.Schema.Create(context.Background()); err != nil {
-        log.Fatalf("failed creating schema resources: %v", err)
-    }
+	client, err := ent.Open("postgres", "localhost port=5432 user=user dbname=remember password=password")
+
+	if err != nil {
+		log.Fatalf("failed connecting to mysql: %v", err)
+	}
+	defer client.Close()
+	ctx := context.Background()
+	if err := client.Schema.WriteTo(ctx, os.Stdout); err != nil {
+		log.Fatalf("failed printing schema changes: %v", err)
+	}
 }
