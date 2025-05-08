ea81c0ee94b41feff75ecf9495e1db9efec39da1
KaranJayakumar <karanjaykumar@gmail.com>
2025-05-08 22:31:21 +1000

    Add basic sign in screens

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,24 +1,18 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
+import { useAuth } from '@clerk/clerk-expo'
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, setLoaded] = useState(true)
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+  SplashScreen.hideAsync();
 
-  if (!loaded) {
-    return null;
-  }
-
-  return(
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
   )
 }
