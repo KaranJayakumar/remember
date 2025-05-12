6ad57ee2e66e047dacce8c14b1b6c49e61da88a7
KaranJayakumar <karanjaykumar@gmail.com>
2025-05-12 20:49:21 +1000

    Start build of sign up page

--- frontend/app/auth/index.tsx
+++ frontend/app/auth/index.tsx
@@ -1,10 +1,57 @@
-import { View, Text } from 'react-native'
-export default function AuthPage() {
+import { Text, View, TextInput, Alert } from "react-native"
+import { useForm, Controller } from "react-hook-form"
+import Button from "../../components/core/Button"
+
+export default function SignIn() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  const onSubmit = () => {
+
+  }
   return (
-    <View>
-      <Text style={{ fontSize: 20, color: 'black' }}>
-        Hello, world!
-      </Text>
+    <View className="flex-1 justify-center items-center">
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Email"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+      />
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            secureTextEntry={true}
+            placeholder="Password"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="password"
+      />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   )
 }
