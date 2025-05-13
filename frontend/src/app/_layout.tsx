01613903f237ae974721362fb63d6140fc2a70d6
KaranJayakumar <karanjaykumar@gmail.com>
2025-05-13 22:55:39 +1000

    Add react query to project

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -2,9 +2,14 @@ import { Slot } from 'expo-router';
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import * as SplashScreen from 'expo-splash-screen';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import "./global.css"
 
 SplashScreen.preventAutoHideAsync();
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
   console.log("Reached root layout")
@@ -12,7 +17,9 @@ export default function RootLayout() {
 
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Slot />
+      <QueryClientProvider client={queryClient}>
+        <Slot />
+      </QueryClientProvider>
     </ClerkProvider>
   )
 }
