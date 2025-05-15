72f0043b6603ff87ef1af35e95af82dcb478b7d7
KaranJayakumar <karanjaykumar@gmail.com>
2025-05-16 00:17:17 +1000

    Fix login bug using useEffect in index.tsx

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,13 +1,26 @@
 import { useAuth } from '@clerk/clerk-expo'
-import { Redirect, Stack } from 'expo-router'
+import { useRouter } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
 
 export default function Page() {
-  const { isSignedIn } = useAuth()
+  const { isSignedIn, isLoaded } = useAuth()
+  const router = useRouter()
 
-  if (!isSignedIn) {
-    return <Redirect href={'/auth'} />
-  } else {
-    return <Redirect href={'/home'} />
-  }
+  useEffect(() => {
+    if (!isLoaded) return
+
+    SplashScreen.hideAsync()
+
+    if (isSignedIn) {
+      console.log("Signed in → navigating to /tabs")
+      router.replace("/(tabs)")
+    } else {
+      console.log("Not signed in → navigating to /auth")
+      router.replace("/(auth)")
+    }
+  }, [isLoaded, isSignedIn])
+
+  return null
 }
 
