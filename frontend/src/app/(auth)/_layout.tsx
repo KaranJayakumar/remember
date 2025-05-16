74c035b1e96d28c0326dd148f5eb7972036e660b
KaranJayakumar <karanjaykumar@gmail.com>
2025-05-16 20:32:10 +1000

    Add effect to redirect on login success

--- frontend/src/app/(auth)/_layout.tsx
+++ frontend/src/app/(auth)/_layout.tsx
@@ -1,5 +1,14 @@
-import { Slot } from 'expo-router'
+import { useAuth } from '@clerk/clerk-expo'
+import { Slot, useRouter } from 'expo-router'
+import { useEffect } from 'react'
 
 export default function AuthRoutesLayout() {
+  const { isSignedIn } = useAuth()
+  const router = useRouter()
+  useEffect(() => {
+    if (isSignedIn) {
+      router.replace('/(tabs)')
+    }
+  }, [isSignedIn])
   return <Slot />
 }
