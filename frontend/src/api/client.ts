b009878440a2f6a64579c6956f964d182c939ea2
KaranJayakumar <karanjaykumar@gmail.com>
2025-05-16 20:34:32 +1000

    Add per request logging for debugging

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,9 +1,21 @@
-import { useAuth } from '@clerk/clerk-expo';
-import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 const client = axios.create({
-  baseURL: `${process.env.EXPO_BASE_API_URL}`,
+  baseURL: `${process.env.EXPO_PUBLIC_BASE_API_URL}`,
   timeout: 1000,
+});
+
+client.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  console.log("🚀 REQUEST:");
+  const url = `${config.baseURL ?? ''}${config.url ?? ''}`;
+  console.log("URL:", url);
+  console.log("Method:", config.method);
+  console.log("Headers:", config.headers);
+  console.log("Data:", config.data);
+  return config;
+}, (error) => {
+  console.error("❌ REQUEST ERROR:", error);
+  return Promise.reject(error);
 });
 
 export default client;
