f3d7918eac98a92c4a169d5a0beff2f4b8aa12b5
KaranJayakumar <karanjaykumar@gmail.com>
2025-05-17 16:52:53 +1000

    Use experimental resource cache on clerk for offline support

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Slot } from 'expo-router';
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import * as SplashScreen from 'expo-splash-screen';
+import { resourceCache } from '@clerk/clerk-expo/resource-cache'
 import {
   QueryClient,
   QueryClientProvider,
@@ -14,7 +15,7 @@ const queryClient = new QueryClient()
 export default function RootLayout() {
 
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider tokenCache={tokenCache} __experimental_resourceCache={resourceCache}>
       <QueryClientProvider client={queryClient}>
         <Slot />
       </QueryClientProvider>
