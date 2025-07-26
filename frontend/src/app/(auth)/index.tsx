57d83aef184722166cd4fa0d2d07840851bd3e3a
KaranJayakumar <karanjaykumar@gmail.com>
2025-07-26 13:44:21 +1000

    Add google sign in button

--- frontend/src/app/(auth)/index.tsx
+++ frontend/src/app/(auth)/index.tsx
@@ -1,49 +1,61 @@
-import React, { useCallback, useEffect } from 'react'
-import * as WebBrowser from 'expo-web-browser'
-import * as AuthSession from 'expo-auth-session'
-import { useSSO } from '@clerk/clerk-expo'
-import { View, Button } from 'react-native'
+import React, { useCallback, useEffect } from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as AuthSession from "expo-auth-session";
+import { useSSO } from "@clerk/clerk-expo";
+import { View } from "react-native";
+import { Button } from "../../components/ui/button";
+import { Text } from "../../components/ui/text";
+import GoogleIcon from "../../components/icons/google";
+import { Platform } from "react-native";
 
 export const useWarmUpBrowser = () => {
   useEffect(() => {
-    void WebBrowser.warmUpAsync()
-    return () => {
-      void WebBrowser.coolDownAsync()
+    if (Platform.OS !== "web") {
+      void WebBrowser.warmUpAsync();
+      return () => {
+        void WebBrowser.coolDownAsync();
+      };
     }
-  }, [])
-}
+  }, []);
+};
 
-WebBrowser.maybeCompleteAuthSession()
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Page() {
-  useWarmUpBrowser()
+  useWarmUpBrowser();
 
-  const { startSSOFlow } = useSSO()
+  const { startSSOFlow } = useSSO();
 
   const onPress = useCallback(async () => {
     try {
-      console.log("Pressed sign in")
-      const { createdSessionId, setActive, signIn, signUp } = await startSSOFlow({
-        strategy: 'oauth_google',
-        redirectUrl: AuthSession.makeRedirectUri(),
-      })
-      console.log("Reached post")
+      console.log("Pressed sign in");
+      const { createdSessionId, setActive, signIn, signUp } =
+        await startSSOFlow({
+          strategy: "oauth_google",
+          redirectUrl: AuthSession.makeRedirectUri(),
+        });
 
       if (createdSessionId) {
-
-        console.log("Created session")
-
-        setActive!({ session: createdSessionId })
+        setActive!({ session: createdSessionId });
       } else {
       }
     } catch (err) {
-      console.error(JSON.stringify(err, null, 2))
+      console.error(JSON.stringify(err, null, 2));
     }
-  }, [])
+  }, []);
 
   return (
     <View className="flex-1 justify-center items-center">
-      <Button title="Sign in with Google" onPress={onPress} />
+      <Button
+        onPress={onPress}
+        variant={"outline"}
+        className="flex-row items-center justify-center"
+      >
+        <View className="w-6 h-6 items-center justify-center">
+          <GoogleIcon />
+        </View>
+        <Text className="ml-2">Sign in with Google</Text>
+      </Button>
     </View>
-  )
+  );
 }
