942d11607940194b4900c7dc2ca55296e013ce51
KaranJayakumar <karanjaykumar@gmail.com>
2025-07-28 00:45:16 +1000

    Test : Add PortalHost to app layout to test rendering

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,23 +1,21 @@
-import { Slot } from 'expo-router';
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import * as SplashScreen from 'expo-splash-screen';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import "./global.css"
+import { Slot } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import * as SplashScreen from "expo-splash-screen";
+import { PortalHost } from "@rn-primitives/portal";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./global.css";
 
 SplashScreen.preventAutoHideAsync();
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <QueryClientProvider client={queryClient}>
         <Slot />
       </QueryClientProvider>
+      <PortalHost />
     </ClerkProvider>
-  )
+  );
 }
