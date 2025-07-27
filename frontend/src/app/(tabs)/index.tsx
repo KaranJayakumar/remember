51d6b9c218fc276ac11d3f0c10d5a1b3ca9a0ae2
KaranJayakumar <karanjaykumar@gmail.com>
2025-07-28 00:42:06 +1000

    Temp : Add testing dialog to tabs page

--- frontend/src/app/(tabs)/index.tsx
+++ frontend/src/app/(tabs)/index.tsx
@@ -6,6 +6,16 @@ import { createConnectionApi } from "../../api/api";
 import { useState } from "react";
 import { Text } from "../../components/ui/text";
 import { Button } from "../../components/ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogClose,
+  DialogTitle,
+  DialogFooter,
+  DialogHeader,
+  DialogDescription,
+  DialogContent,
+} from "../../components/ui/dialog";
 
 export default function Homepage() {
   const { connections = [] } = useConnections();
@@ -30,14 +40,32 @@ export default function Homepage() {
           </Text>
         </View>
       ))}
-
       <Text className="mb-4">Hello there, welcome to remember</Text>
-      <Button
-        variant={"default"}
-        className="flex-row items-center justify-center"
-      >
-        <Text>Create a Connection</Text>
-      </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant={"default"}
+            className="flex-row items-center justify-center"
+          >
+            <Text>Create a Connection</Text>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button>
+                <Text>OK</Text>
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </View>
   );
 }
