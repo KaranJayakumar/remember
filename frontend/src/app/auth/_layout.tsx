086a5ffaebd798e6ac6ddc7045ecb7dc0e85930f
KaranJayakumar <karanjaykumar@gmail.com>
2025-12-20 23:52:26 +1100

    WIP : add nav ready check

--- frontend/src/app/auth/_layout.tsx
+++ frontend/src/app/auth/_layout.tsx
@@ -1,17 +1,25 @@
 import { useAuth } from '@clerk/clerk-expo'
 import { Slot, useRouter } from 'expo-router'
 import { useEffect } from 'react'
+import { useRootNavigationState } from 'expo-router'
 
 export default function AuthRoutesLayout() {
   const { isSignedIn } = useAuth()
   const router = useRouter()
+  const rootNavigationState = useRootNavigationState()
+  const navigatorReady = rootNavigationState?.key != null
+
   useEffect(() => {
+    if (!navigatorReady){
+      return
+    }
     if (isSignedIn) {
       router.replace('/(tabs)')
     }
-    if (isSignedIn) {
-      router.replace('/(auth)/sign-in')
+    if (!isSignedIn) {
+      router.replace('/auth/sign-in')
     }
-  }, [isSignedIn])
+  }, [isSignedIn, navigatorReady])
+
   return <Slot />
 }
--- frontend/src/app/auth/sign-in.tsx
+++ frontend/src/app/auth/sign-in.tsx
@@ -21,32 +21,9 @@ WebBrowser.maybeCompleteAuthSession();
 
 export default function Page() {
   useWarmUpBrowser();
-
-  const { startSSOFlow } = useSSO();
-  const googleLogoPath = require("../../../assets/svg/google-main-icon.svg");
-
-  const onPress = useCallback(async () => {
-    try {
-      console.log("Pressed sign in");
-      const { createdSessionId, setActive, signIn, signUp } =
-        await startSSOFlow({
-          strategy: "oauth_google",
-          redirectUrl: AuthSession.makeRedirectUri(),
-        });
-
-      if (createdSessionId) {
-        setActive!({ session: createdSessionId });
-      } else {
-      }
-    } catch (err) {
-      console.error(JSON.stringify(err, null, 2));
-    }
-  }, []);
-
   return (
     <View className="flex-1 justify-center items-center">
       <SignInForm/>
     </View>
   );
 }
-
